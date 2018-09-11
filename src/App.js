64234e8bec55bedb081cd1e4752d3e218ff2fa0f
Elijah <elijah.abella@aol.com>
2018-09-11 14:31:30 -0700

    added lists

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import CartFooter from './CartFooter';
 import CartItems from './CartItems';
 
 const App = () => {
+    let cartItemsList = [
+        { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+        { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+        { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+    ]
+
     return (
         <div>
             <CartHeader/>
-            <CartItems/>
+            <CartItems items={cartItemsList}/>
             <CartFooter copyright={'2018'}/>
         </div>
     )
